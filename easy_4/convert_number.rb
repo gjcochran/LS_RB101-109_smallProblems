a8d67674943b10444cfc96a81a4eaa5f19ed8c6d
Garrett Cochran <garrettjcochran@gmail.com>
2022-11-17 22:25:40 -0700

    11.17.22

--- easy_4/convert_number.rb
+++ easy_4/convert_number.rb
@@ -9,15 +9,42 @@
 
 # D/A
 # create a hash that is the opposite of previous problems
-DIGITS = {
-  0 => "0",
-  1 => "1",
-  2 => "2",
-  3 => "3",
-  4 => "4",
-  5 => "5",
-  6 => "6",
-  7 => "7",
-  8 => "8",
-  9 => "9",
-}
+# step 1 -- turn into array (w divmod method)
+# step 2 -- pop off 2nd element in returned array
+# step 3 -- convert array to string with join method
+
+def integer_to_string(int)
+  arr = int.divmod(1)
+  arr.pop
+  arr.join
+end
+
+p integer_to_string(4321) == "4321"
+p integer_to_string(0) == "0"
+p integer_to_string(5000) == "5000"
+
+# works, although reviewing prompt i wonder if they consider Array#join a standard conversion method...
+
+# LS answer typed out below
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string_v2(number)
+  result = ''
+  loop do 
+    number, remainder = number.divmod(10)
+    result.prepend(DIGITS[remainder])
+    break if number == 0
+  end
+  result
+end
+
+# p DIGITS[1] #=> '1'
+# irb(main):044:0> result = ''
+# => ""
+# irb(main):045:0> number = 4321
+# => 4321
+# irb(main):046:0> number, remainder = number.divmod(10)
+# => [432, 1]
+# irb(main):047:0> result.prepend('1')
+# => "1"
--- easy_5/letter_counter_1.rb
+++ easy_5/letter_counter_1.rb
@@ -14,12 +14,49 @@
 # then iterate over the array
 # each element in array -- size of el is analyzed with String#size
 
+
+# def word_sizes(str)
+#   result = {}
+#   arr = str.split
+#   arr.map do |word| 
+#     counter = 1
+#     result[word.length] = counter
+#     counter += 1 if result.key?(word.length)
+#   end
+#   result.sort.to_h
+# end
+
+# p word_sizes("Four score and seven.")
+# p word_sizes("Hey diddle diddle, the cat and the fiddle!")
+
+#=>
+# {3=>1, 4=>1, 5=>1, 6=>1}
+# {3=>1, 6=>1, 7=>1}
+
+# am very close but cant figure out how to set the value to be the dynamic number of occurrences of words of key length
+
+# https://ruby-doc.org/core-3.1.2/Enumerable.html#method-i-group_by
+
 def word_sizes(str)
-  out = {}
   arr = str.split
-  arr.each { |sub| out[sub.size] = arr.count(sub.size) }
-  out
+  result = arr.group_by { |el| el.length }.transform_values(&:size)
+  result.sort.to_h
 end
 
 p word_sizes("Four score and seven.")
+p word_sizes("Hey diddle diddle, the cat and the fiddle!")
+#==> 
+# {3=>1, 4=>1, 5=>1, 6=>1}
+# {3=>5, 6=>1, 7=>2}
+
+# mine worked, but took me a long time to solve and ended up having to use two methods that was previously unfamilar with (group_by && tranform_values). good to know how to use them now, but also not realistic for test environment. turns out i just had to set the value to += 1 within an iterator method! so simple smh.
+
+# LS answer below
+def word_sizes_v2(words_string)
+  counts = Hash.new(0)
+  words_string.split.each do |word|
+    counts[word.size] += 1
+  end
+  counts
+end
 p word_sizes("Hey diddle diddle, the cat and the fiddle!")
