541d20467005aec19a0c4b75567e2731aeb5f31d
Garrett Cochran <garrettjcochran@gmail.com>
2022-12-01 15:06:16 -0700

    12.01.22 #1

--- medium_1/stack_machine_interpretation.rb
+++ medium_1/stack_machine_interpretation.rb
@@ -21,3 +21,57 @@
 
 # You should initialize the register to 0.
 
+#  did not understand this one at all, LS solution written out below
+
+def minilang(program)
+  stack = []
+  register = 0
+
+  program.split.each do |token|
+    case token
+    when 'ADD' then register += stack.pop
+    when 'DIV' then register /= stack.pop
+    when 'MULT' then register *= stack.pop
+    when 'MOD' then register %= stack.pop
+    when 'SUB' then register -= stack.pop
+    when 'PUSH' then stack.push(register)
+    when 'POP' then register = stack.pop
+    when 'PRINT' then puts register 
+    else
+      register = token.to_i
+    end
+  end
+end
+
+# tests
+p minilang('PRINT')
+# 0
+
+p minilang('5 PUSH 3 MULT PRINT')
+# 15
+
+p minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# 5
+# 3
+# 8
+
+p minilang('5 PUSH POP PRINT')
+# 5
+
+p minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# 5
+# 10
+# 4
+# 7
+
+p minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# 6
+
+p minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# 12
+
+p minilang('-3 PUSH 5 SUB PRINT')
+# 8
+
+p minilang('6 PUSH')
+# (nothing printed; no PRINT commands)
